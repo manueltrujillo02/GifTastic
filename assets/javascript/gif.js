6e9e545c4d2ee7b5d0ad8483f809130c51ada923
Bat <noreply>
2019-03-06 00:40:32 -0800

    homework git commit -m homework git commit -m homework

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -3,37 +3,73 @@
 
 
 
-$("button").on("click", function () {
+    $("button").on("click", function () {
 
-    var animal = $(this).attr("data-animal");
+        var animal = $(this).attr("data-animal");
+        var apiKey = "vWoBLrCiGsRi0lemZntNZWj6gFOaIiZx";
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + animal + "&limit=10&offset=0&rating=G&lang=en";
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+        
+            var results = response.data;
+            
 
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=vWoBLrCiGsRi0lemZntNZWj6gFOaIiZx&q=" + animal + "&limit=10&offset=0&rating=G&lang=en";
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response)
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div>");
-            var animalImage = $("<img>");
+            
+            for (var i = 0; i < results.length; i++) {
+                
+                var image = $("<img>");
 
-            // Giving the image tag an src attribute of a proprty pulled off the
-            // result item
-            animalImage.attr("src", results[i].images.fixed_height.url);
+            
+                image.attr("src", results[i].images.fixed_height.url);
+                console.log( results[i].images.fixed_height.url)
 
-            // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                $("#divDisplay").prepend(image);
+                    console.log(image)
+                
+            };
+        });
+    
+  });
+    // function renderButtons() {
 
-            gifDiv.append(animalImage);
+    //     // Deleting the movies prior to adding new movies
+    //     // (this is necessary otherwise you will have repeat buttons)
+    //     $("#buttons-view").empty();
 
-            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-            $("#gifs-appear-here").prepend(gifDiv);
-            //     var animalImage = $("<img>");
-            //     animalImage.attr("src", results[i].images.fixed_height.url);
-            //     console.log(results[i].images.fixed_height.url);
-            //     gifDiv.prepend(animalImage);
-            //     $("#divDisplay").prepend(gifDiv)
-            // }
-        };
-    });
-});
+    //     // Looping through the array of movies
+    //     for (var i = 0; i < movies.length; i++) {
+
+    //       // Then dynamicaly generating buttons for each movie in the array
+    //       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    //       var a = $("<button>");
+    //       // Adding a class of movie-btn to our button
+    //       a.addClass("movie-btn");
+    //       // Adding a data-attribute
+    //       a.attr("data-name", movies[i]);
+    //       // Providing the initial button text
+    //       a.text(movies[i]);
+    //       // Adding the button to the buttons-view div
+    //       $("#buttons-view").append(a);
+    //     }
+    //   }
+
+    //   // This function handles events where a movie button is clicked
+    //   $("#add-movie").on("click", function(event) {
+    //     event.preventDefault();
+    //     // This line grabs the input from the textbox
+    //     var movie = $("#movie-input").val().trim();
+
+    //     // Adding movie from the textbox to our array
+    //     movies.push(movie);
+
+    //     // Calling renderButtons which handles the processing of our movie array
+    //     renderButtons();
+    //   });
+
+    //   // Adding a click event listener to all elements with a class of "movie-btn"
+    //   $(document).on("click", ".movie-btn", displayMovieInfo);
+
+    //   // Calling the renderButtons function to display the intial buttons
+    //   renderButtons();
